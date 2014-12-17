0087d19c68a7e7b5fa47c0a1e33cb5d34a21ca5f
Nebojša Jakovljević <nebojsa@nebjak.net>
2014-12-17 07:59:33 +0100

    cleaned and documented code

--- lib/serbia_zip_codes.rb
+++ lib/serbia_zip_codes.rb
@@ -1,18 +1,21 @@
 require 'yaml'
 
-class SerbiaZipCodes
-  def self.load_data
-    @data = YAML.load_file File.join(File.dirname(__FILE__),"data/serbia_zip_codes.yaml")
-  end
+module SerbiaZipCodes
+  @data = YAML.load_file File.join(File.dirname(__FILE__),"data/serbia_zip_codes.yaml")
 
+  # Search for city by zip code
+  #
+  # @param zip [String] zip code in string format
+  # @return [Array] result as array of hashes
   def self.find_by_zip(zip = nil)
-    load_data
     @data.select { |d| d["zip_code"].match /#{zip}/i }
   end
 
-  
+  # Search for zip code by city
+  #
+  # @param city [String] city name in string format
+  # @return [Array] result as array of hashes
   def self.find_by_city(city = nil)
-    load_data
     @data.select { |d| d["city"].match /#{city}/i }
   end
 end
--- serbia_zip_codes.gemspec
+++ serbia_zip_codes.gemspec
@@ -4,7 +4,7 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 Gem::Specification.new do |spec|
   spec.name          = "serbia_zip_codes"
-  spec.version       = "0.0.2"
+  spec.version       = "0.1.0"
   spec.authors       = ["Nebojša Jakovljević"]
   spec.email         = ["nebojsa@nebjak.net"]
   spec.summary       = %q{Easy access to Serbia zip codes}
